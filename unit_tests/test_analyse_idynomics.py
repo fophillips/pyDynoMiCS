95b6035b82ffeee73bbde953e5d036c50fa4fa8c
Fred Phillips <fophillips@fophillips.org>
2014-03-10 10:44:33 +0000

    Add unit tests for timesteps and dimensions

--- unit_tests/test_analyse_idynomics.py
+++ unit_tests/test_analyse_idynomics.py
@@ -5,6 +5,8 @@ from os.path import join, dirname, realpath
 class TestAnalyseiDynomics:
     expected_solutes = ['MyAtmos', 'pressure']
     expected_species = ['MyBact']
+    expected_timesteps = 2
+    expected_dimensions = (20.0, 20.0, 2.0)
     
     def setUp(self):
         self.directory = join(dirname(realpath(__file__)), 'test_data')
@@ -20,3 +22,11 @@ class TestAnalyseiDynomics:
     def test_species_names(self):
         actual_species = self.analysis.species_names
         assert_list_equal(self.expected_species, actual_species)
+
+    def test_total_timesteps(self):
+        actual_timesteps = self.analysis.total_timesteps
+        assert_equals(self.expected_timesteps, actual_timesteps)
+
+    def test_world_dimensions(self):
+        actual_dimensions = self.analysis.world_dimensions
+        assert_equal(self.expected_dimensions, actual_dimensions)
