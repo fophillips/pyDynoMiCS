713e720bd3e4029273d72ab58aa79fbd79f0bafa
Fred Phillips <fophillips@fophillips.org>
2014-03-10 10:51:13 +0000

    Add unit test for getting reaction rate names

--- unit_tests/test_analyse_idynomics.py
+++ unit_tests/test_analyse_idynomics.py
@@ -5,6 +5,7 @@ from os.path import join, dirname, realpath
 class TestAnalyseiDynomics:
     expected_solutes = ['MyAtmos', 'pressure']
     expected_species = ['MyBact']
+    expected_reaction_rates = ['MyGrowth-rate']
     expected_timesteps = 2
     expected_dimensions = (20.0, 20.0, 2.0)
     
@@ -23,6 +24,10 @@ class TestAnalyseiDynomics:
         actual_species = self.analysis.species_names
         assert_list_equal(self.expected_species, actual_species)
 
+    def test_reaction_rate_names(self):
+        actual_reaction_rates = self.analysis.reaction_rate_names
+        assert_list_equal(self.expected_reaction_rates, actual_reaction_rates)
+
     def test_total_timesteps(self):
         actual_timesteps = self.analysis.total_timesteps
         assert_equals(self.expected_timesteps, actual_timesteps)
@@ -30,3 +35,5 @@ class TestAnalyseiDynomics:
     def test_world_dimensions(self):
         actual_dimensions = self.analysis.world_dimensions
         assert_equal(self.expected_dimensions, actual_dimensions)
+
+        
